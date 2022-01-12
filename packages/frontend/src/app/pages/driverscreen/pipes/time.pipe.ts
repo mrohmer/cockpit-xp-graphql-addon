83cac21d2173a04482a8c797868b7d9116a65c77
mrohmer <rohmer.matze@gmail.com>
2022-01-12 18:46:07 +0100

    fix(frontend): fix time pipe with undefined input

--- packages/frontend/src/app/pages/driverscreen/pipes/time.pipe.ts
+++ packages/frontend/src/app/pages/driverscreen/pipes/time.pipe.ts
@@ -5,7 +5,10 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class TimePipe implements PipeTransform {
 
-  transform(value: number): string {
+  transform(value: number): string|null {
+    if (!value) {
+      return null;
+    }
     const millis = (value % 1000);
     const sec = String(Math.floor(value / 1000) % 60).padStart(2, '0');
     const min = Math.floor(value / 60000);
