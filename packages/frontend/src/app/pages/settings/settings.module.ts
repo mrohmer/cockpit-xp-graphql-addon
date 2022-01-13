db52a84a753947dd91d0f91bcfbb7f1c60182fcb
mrohmer <rohmer.matze@gmail.com>
2022-01-13 22:57:12 +0100

    feat(frontend): style settings

--- packages/frontend/src/app/pages/settings/settings.module.ts
+++ packages/frontend/src/app/pages/settings/settings.module.ts
@@ -4,15 +4,14 @@ import { CommonModule } from '@angular/common';
 import { SettingsRoutingModule } from './settings-routing.module';
 import { SettingsComponent } from './settings.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import { SettingsRowComponent } from './components/settings-row/settings-row.component';
 import { SettingsHeadlineDirective } from './directives/settings-headline.directive';
+import {SharedModule} from '../../shared/shared.module';
 
 @NgModule({
   declarations: [
     SettingsComponent,
-    SettingsRowComponent,
     SettingsHeadlineDirective,
   ],
-  imports: [CommonModule, SettingsRoutingModule, ReactiveFormsModule],
+  imports: [CommonModule, SettingsRoutingModule, ReactiveFormsModule, SharedModule],
 })
 export class SettingsModule {}
