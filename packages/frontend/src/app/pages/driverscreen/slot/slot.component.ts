951df737922835d953f4fe805034de3c0c50f9b7
mrohmer <rohmer.matze@gmail.com>
2022-01-14 15:22:18 +0100

    fix(frontend): minor fullscreen improvements

--- packages/frontend/src/app/pages/driverscreen/slot/slot.component.ts
+++ packages/frontend/src/app/pages/driverscreen/slot/slot.component.ts
@@ -1,8 +1,8 @@
 import {ChangeDetectionStrategy, Component, Inject, OnDestroy, OnInit, PLATFORM_ID} from '@angular/core';
 import {DynamicUrlApolloService} from '$core/services/dynamic-url-apollo.service';
 import {gql} from 'apollo-angular';
-import {filter, pluck, switchMap, takeUntil, tap} from 'rxjs/operators';
-import {defer, iif, Observable, of, Subject} from 'rxjs';
+import {filter, pluck, startWith, switchMap, takeUntil, tap} from 'rxjs/operators';
+import {combineLatest, defer, fromEvent, iif, merge, Observable, of, Subject} from 'rxjs';
 import {ActivatedRoute} from '@angular/router';
 import {WakeLockService} from '$core/services/wake-lock.service';
 import {Store} from '@ngrx/store';
@@ -13,27 +13,27 @@ import {DOCUMENT, isPlatformBrowser} from '@angular/common';
   selector: 'app-slot',
   templateUrl: './slot.component.html',
   styleUrls: ['./slot.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SlotComponent implements OnInit, OnDestroy {
 
   private destroyed$ = new Subject();
 
   slot$: Observable<Slot> = this.activatedRoute.params
-  .pipe(
-    pluck('slot'),
-    switchMap(slotId => iif(
-      () => !!slotId,
-      this.apollo.subscribe<{ slot: Slot }>({
-        query: SLOT_DETAIL_SUBSCRIPTION,
-        variables: {slotId},
-      }),
-      of({data: {slot: null}})
-    )),
-    tap(console.log),
-    pluck('data'),
-    pluck('slot'),
-  );
+    .pipe(
+      pluck('slot'),
+      switchMap(slotId => iif(
+        () => !!slotId,
+        this.apollo.subscribe<{ slot: Slot }>({
+          query: SLOT_DETAIL_SUBSCRIPTION,
+          variables: {slotId},
+        }),
+        of({data: {slot: null}})
+      )),
+      tap(console.log),
+      pluck('data'),
+      pluck('slot'),
+    );
 
   constructor(
     private apollo: DynamicUrlApolloService,
@@ -54,22 +54,34 @@ export class SlotComponent implements OnInit, OnDestroy {
       )
       .subscribe()
     ;
-    this.store.select('settings', 'autoFullscreen')
-      .pipe(
-        filter(() => isPlatformBrowser(this.platformId)),
-        switchMap((autoFullscreen) => iif(
-          () => autoFullscreen,
-          defer(() => this.document.body.requestFullscreen()),
-          defer(() => this.document.exitFullscreen()),
-        )),
-        takeUntil(this.destroyed$),
-      )
-      .subscribe(
-        () => undefined,
-        () => undefined,
-        () => this.document.exitFullscreen(),
-      )
+    if (isPlatformBrowser(this.platformId)) {
+      combineLatest([
+        this.store.select('settings', 'autoFullscreen'),
+        merge(
+          fromEvent(this.document, 'visibilitychange'),
+          fromEvent(window, 'focus'),
+
+        ).pipe(
+          startWith(true),
+        ),
+      ])
+        .pipe(
+          filter(() => this.document.visibilityState === 'visible'),
+          switchMap(([autoFullscreen]) => iif(
+            () => autoFullscreen,
+            defer(() => this.document.body.requestFullscreen()),
+            defer(() => this.document.exitFullscreen()),
+          )),
+          takeUntil(this.destroyed$),
+        )
+        .subscribe(
+          () => undefined,
+          () => undefined,
+          () => this.document.exitFullscreen(),
+        );
+    }
   }
+
   ngOnDestroy(): void {
     this.destroyed$.next();
     this.destroyed$.complete();
@@ -119,6 +131,7 @@ const SLOT_DETAIL_SUBSCRIPTION = gql`
         }
     }
 `
+
 interface Slot {
   position: number;
   driver: {
