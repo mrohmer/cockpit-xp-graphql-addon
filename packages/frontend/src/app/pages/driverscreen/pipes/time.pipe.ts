be5462230cf28c773a3413e7b1ae52deef8b2767
mrohmer <rohmer.matze@gmail.com>
2022-01-30 18:56:36 +0100

    fix(frontend): pad millis

--- packages/frontend/src/app/pages/driverscreen/pipes/time.pipe.ts
+++ packages/frontend/src/app/pages/driverscreen/pipes/time.pipe.ts
@@ -12,7 +12,7 @@ export class TimePipe implements PipeTransform {
     const millis = (value % 1000);
     const sec = String(Math.floor(value / 1000) % 60).padStart(2, '0');
     const min = Math.floor(value / 60000);
-    return `${min}:${sec}.${millis.toFixed(0)}`;
+    return `${min}:${sec}.${millis.toFixed(0).padStart(3, '0')}`;
   }
 
 }
